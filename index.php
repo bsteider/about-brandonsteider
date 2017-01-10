65bbadbca62dc99f3f21bae762eaee46bc2cb93f
Brandon Steider <bsteider@cnm.edu>
2017-01-10 08:58:25 -0700

    Pushing to guthub

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About Brandon Steider</title>
+	</head>
+	<body>
+		<p>
+			Hi, I'm Brandon Steider. Just a little bit about me, I like a lot of things. I've been very active my whole
+			LIfe.I dirtbike, I snowboard, I skate, I write, I play piano, trumpet, ukulele, and guitar. Music basicaly runs
+			in the family. My dream in life is the be an astrophysicist or a famous writer. Coding is a good career for me
+			considering I have always been around technology, and I really enjoy it.
+		</p>
+	</body>
+</html>
