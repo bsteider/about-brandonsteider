365d0c69a96076c3073bf587bfd624d62ca3834c
ZaK Abad <the.real.zak.neutrino@gmail.com>
2017-01-10 09:41:13 -0700

    updated about brandonsteidner from zabad1

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
 			LIfe.I dirtbike, I snowboard, I skate, I write, I play piano, trumpet, ukulele, and guitar. Music basicaly runs
 			in the family. My dream in life is the be an astrophysicist or a famous writer. Coding is a good career for me
 			considering I have always been around technology, and I really enjoy it. I secretly love comcast and hate when
-			individuals talk badly about it. I also hate Star Trek with a passion. So I don't know how I will be able to get
+			individuals talk badly about it. ALL HAIL COMCAST... I also hate Star Trek with a passion. So I don't know how I will be able to get
 			through this class hearing about this Gene Roddenberry garbage.
+
+			I enjoy a good snack of spam. Spam is the best when it's grilled and glazed with honey. I've always wanted to make
+			a trip to St. Louis.
 		</p>
 	</body>
 </html>
